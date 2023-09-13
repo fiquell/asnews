81eb59b82d9823c6076867e2483acc5a0712de08
Fiquell <fiquellh@gmail.com>
2023-09-13 20:36:01 +0800

    rewrite all code

---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+const config = {
+  bracketSameLine: true,
+  semi: false,
+  trailingComma: "none"
+}
+
+export default config
---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import("tailwindcss").Config} */
+const config = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
+
+export default config
